8cf2f39ffe9456089bc330e3c798cb9cc97e43bd
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-10-19 17:53:32 -0700

    udemy start

---
+++ 1-basics/basics.py
@@ -0,0 +1,21 @@
+
+
+print('hello world')
+
+print('what is your name?')
+
+userName = input()
+
+print('thanks, ' + userName + '!')
+
+print('the length of your name is:')
+
+print(len(userName))
+
+print('what is your age?')
+
+userAge = input()
+
+print('you will be ' + str(int(userAge) + 1) + ' in one year')
+
+
---
+++ README.md
@@ -0,0 +1,13 @@
+# [Automate the Boring Stuff With Python Programming - Al Sweigart (Udemy)](https://www.udemy.com/course/automate/)
+
+
+# Table of Contents
+
+### 1-basics
+- [basics](1-basics/basics.py)
+
+
+
+
+
+
