a9eb9d68cd57a160696574e0a754bc63d8818996
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-10-20 18:49:46 -0700

    if else

---
+++ 2_flow_control/if_else.py
@@ -0,0 +1,32 @@
+password = 'swordfish'
+
+if password == 'swordfish':
+    print('access granted')
+else:
+    print('access denied')
+
+# ---
+
+name = 'bob'
+age = 3000
+
+if name = 'alice':
+    print('hi alice')
+elif age < 12:
+    print('you are not alice')
+elif age > 2000:
+    print('alice is not a vampire')
+elif age > 100:
+    print('grannie')
+
+# ---
+
+print('enter a name')
+name = input()
+
+if name != '':
+    print('thank you for entering a name')
+else:
+    print('you did not enter a name')
+
+
--- README.md
+++ README.md
@@ -4,10 +4,12 @@
 # Table of Contents
 
 ### 1-basics
-- [basics](1-basics/basics.py)
+- [basics](1_basics/basics.py)
 
+### 2-flow-control
+- [if-else](2_flow_control/if_else.py)
 
-
+### 3-funtions
 
 
 
